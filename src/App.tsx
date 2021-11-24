b15116d017d6deff544f32649f1435be96299b04
Paul Jeon <pauljeonn@gmail.com>
2021-11-24 23:09:55 +0900

    create types for data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
 import { useState, useEffect } from 'react';
 
+type Country = {
+	Country: string;
+	CountryCode: string;
+	Date: string;
+	ID: string;
+	NewConfirmed: number;
+	NewDeaths: number;
+	NewRecovered: number;
+	Premium: unknown;
+	Slug: string;
+	TotalConfirmed: number;
+	TotalDeaths: number;
+	TotalRecovered: number;
+};
+
+type GlobalData = {
+	Date: string;
+	NewConfirmed: number;
+	NewDeaths: number;
+	NewRecovered: number;
+	TotalConfirmed: number;
+	TotalDeaths: number;
+	TotalRecovered: number;
+};
+
+type ResponseData = {
+	Countries: Country[];
+	Date: string;
+	Global: GlobalData;
+	ID: string;
+	Message: string;
+};
+
 const App: React.FunctionComponent = () => {
-	const [data, setData] = useState();
+	const [data, setData] = useState<ResponseData | undefined>();
 
 	const fetchData = async () => {
 		const result = await fetch('https://api.covid19api.com/summary');
-		const data = await result.json();
+		const data: ResponseData = await result.json();
 
 		setData(data);
 		console.log(data);

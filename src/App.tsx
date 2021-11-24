bae7e2533a8e6737c16852973d8bb251c45ecca1
Paul Jeon <pauljeonn@gmail.com>
2021-11-24 16:40:08 +0900

    fetch covid-19 summary data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,20 @@
+import { useState, useEffect } from 'react';
+
 const App: React.FunctionComponent = () => {
+	const [data, setData] = useState();
+
+	const fetchData = async () => {
+		const result = await fetch('https://api.covid19api.com/summary');
+		const data = await result.json();
+
+		setData(data);
+		console.log(data);
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
 	return (
 		<div className="App">
 			<h1>코로나19(COVID-19) 세계 현황</h1>

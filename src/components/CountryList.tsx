1d9f88af914ae527f8ee3998d364260e60c85a33
Paul Jeon <pauljeonn@gmail.com>
2021-11-26 03:38:27 +0900

    add styling to CountryList

--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -1,17 +1,46 @@
+import styled from '@emotion/styled';
 import { Country } from '../types';
 
 interface Props {
 	countries: Country[];
 }
 
+const ListWrapper = styled.ul`
+	padding: 0;
+	display: flex;
+	flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+	list-style-type: none;
+	flex: 0 1 50%;
+	text-align: center;
+
+	@media (min-width: 420px) {
+		flex: 0 0 33.33%;
+	}
+`;
+
+const ListContent = styled.div`
+	background-color: lightgray;
+	margin: 10px;
+	padding: 10px 0;
+`;
+
 const CountryList: React.FunctionComponent<Props> = ({ countries }) => {
 	return (
-		<ul>
+		<ListWrapper>
 			{countries.map((country) => (
-				<li key={country.ID}>{country.Country}</li>
+				<ListItem key={country.ID}>
+					<ListContent>
+						<h4>{country.Country}</h4>
+						<div>New Confirmed: {country.NewConfirmed}</div>
+						<div>New Deaths: {country.NewDeaths}</div>
+						<div>New Recovered: {country.NewRecovered}</div>
+					</ListContent>
+				</ListItem>
 			))}
-			<li></li>
-		</ul>
+		</ListWrapper>
 	);
 };
 
